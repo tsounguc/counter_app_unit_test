9898ec4fca5126d136a910e727ef63f51574854c
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-02 23:59:53 -0500

    Write more test

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -6,11 +6,23 @@ void main() {
   test(
       'given counter class when it is instantiated then value of count should be 0',
       () {
-        // Arrange
-        final Counter counter = Counter();
-        // Act
-        final val = counter.count;
-        // Assert
-        expect(val, 0);
-      });
+    // Arrange
+    final Counter counter = Counter();
+    // Act
+    final val = counter.count;
+    // Assert
+    expect(val, 0);
+  });
+
+  test(
+      'given counter class when it is incremented then the value of count should be 1',
+      () {
+    // Arrange
+    final Counter counter = Counter();
+    // Act
+    counter.incrementCounter();
+    final val = counter.count;
+    // Assert
+    expect(val, 1);
+  });
 }
