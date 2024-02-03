37bcaf3240f68502a90d6b69b8a26394c9222500
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-03 02:07:46 -0500

    use setUp() to instantiate counter variable

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,13 +2,21 @@ import 'package:counter_app_unit_test/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  late Counter counter;
+  // Pretest
+  // setUp(() => null);
+  // setUpAll(() => null);
+  setUp(() {
+    // Arrange
+    counter = Counter();
+  });
+
+  // Testing
   group('Counter Class - ', () {
     // Test description convention is given...when...then...
     test(
         'given counter class when it is instantiated then value of count should be 0',
         () {
-      // Arrange
-      final Counter counter = Counter();
       // Act
       final val = counter.count;
       // Assert
@@ -18,8 +26,6 @@ void main() {
     test(
         'given counter class when it is incremented then the value of count should be 1',
         () {
-      // Arrange
-      final Counter counter = Counter();
       // Act
       counter.incrementCounter();
       final val = counter.count;
@@ -30,8 +36,6 @@ void main() {
     test(
         'given counter class when it is decremented then the value of count should be -1',
         () {
-      // Arrange
-      final Counter counter = Counter();
       // Act
       counter.decrementCounter();
       final val = counter.count;
@@ -39,4 +43,8 @@ void main() {
       expect(val, -1);
     });
   });
+
+  // Post Test
+  // tearDown(() => null);
+  // tearDownAll(() => null);
 }
